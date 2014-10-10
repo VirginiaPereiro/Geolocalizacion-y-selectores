4afcfb23a797832e47496be3e945d8209d2e907d
VirginiaPereiro <vir_pereiro@hotmail.com>
2014-10-10 13:58:10 +0200

    html5 y JS drag and drop

--- Apis_html5/js/js1.js
+++ Apis_html5/js/js1.js
@@ -1,6 +1,21 @@
 ﻿//variable global
 var datos = [];
 
+//Drag and drop, para elementos de seleccion es muy interesante, sólo funciona con ratón por lo tanto con tablet y móvil no se podrá usar
+function drag(evt) {
+    evt.dataTransfer.setData("text/html", evt.target.id);
+}
+
+function dropable(evt) {
+    evt.preventDefault();//significa que le indicamos que no actúes como predefinido
+}
+
+function drop(evt) {
+    evt.preventDefault();
+    var datos = evt.dataTransfer.getData("text/html");
+    evt.target.appendChild(document.getElementById(datos));//con esto hacemos que se pueda meter dentro
+}
+
 function posicion() {
     if (navigator.geolocation) { //para saber si puede acceder a la geolocalizacion
         //bien por el navegador web o por el usuario
@@ -22,8 +37,7 @@ function posicion() {
         document.querySelector("#longitud").innerHTML = "";
         document.querySelector('#error').innerHTML = "Sin acceso";
     }
-
-    }
+ }
 
 function leerInfo() {
     if (localStorage && localStorage["datos"]) {//si existe
@@ -36,7 +50,6 @@ function leerInfo() {
 
             item.appendChild(texto);
             lista.appendChild(item);
-
         }
 
     } else {
@@ -57,6 +70,18 @@ function escribir() {
     localStorage["datos"] = JSON.stringify(datos);
 }
 
+function dibujar() {
+    var micanvas = document.querySelector("#micanvas");
+    var c = micanvas.getContext("2d");
+
+    c.fillStyle = "#05ac33";//fillStyle es una propiedad
+    c.fillRect(5, 5, 60, 30);//dibuja un rectángulo
+
+    c.fillStyle="#0067cb";
+    c.arc(70, 50, 70, 0, 2 * Math.PI);//dibuja una circunferencia
+    c.stroke();
+}
+
 (function () {
     var ele = document.querySelectorAll('.dibujo');//hace lo mismo que jQuery
     //var ele = document.querySelectorAll('#dibujo');//no valdría porque es para un array
@@ -69,7 +94,14 @@ function escribir() {
         //hace lo mismo que el bind en jquery
     }
     document.querySelector("#btnGuardar").addEventListener("click", escribir);
+
+    //Drag and drop
+    document.querySelector("#dragame").addEventListener("dragstart", drag);
+    document.querySelector("#dropeame").addEventListener("dragover", dropable);
+    document.querySelector("#dropeame").addEventListener("drop", drop);
+
     posicion();
     leerInfo();
+    dibujar();
 
 })();
